5eb3d6a57dde8861a15ee9ff5ea28759131e8e54
A7medK7aled1e <akhaledgamal7@gmail.com>
2025-04-15 20:02:42 +0200

    Refactor HomeViewBody to use CustomAppBar and add buttons for QR code actions

--- lib/views/Home/home_view_body.dart
+++ lib/views/Home/home_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/custom_app_bar.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
@@ -7,16 +9,50 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "QR Code",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 24,
-            ),
+        appBar: CustomAppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomElvetedButton(
+                    text: "Scan QR Code",
+                    onPressed: () {},
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  CustomElvetedButton(
+                    text: "Generate QR Code",
+                    onPressed: () {},
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class CustomElvetedButton extends StatelessWidget {
+  const CustomElvetedButton({
+    super.key,
+    this.onPressed,
+    required this.text,
+  });
+
+  final void Function()? onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(text),
     );
   }
 }
---
+++ lib/views/Home/widgets/custom_app_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({
+    super.key,
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        "QR Code",
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+        ),
+      ),
+    );
+  }
+}
