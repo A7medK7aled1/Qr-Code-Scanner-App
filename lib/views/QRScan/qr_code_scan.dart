62367a85511c0cec3382a1a4e6816e4f512db7f6
A7medK7aled1e <akhaledgamal7@gmail.com>
2025-04-16 21:35:55 +0200

    Add camera and flashlight permissions, implement QR code scanning functionality

--- lib/views/QRScan/qr_code_scan.dart
+++ lib/views/QRScan/qr_code_scan.dart
@@ -1,4 +1,11 @@
+import 'dart:developer';
+
+import 'package:barcode_scan2/platform_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/views/Home/home_view_body.dart';
+import 'package:flutter_application_1/views/Home/widgets/custom_app_bar.dart';
+// import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class QrCodeScan extends StatefulWidget {
   const QrCodeScan({super.key});
@@ -8,8 +15,46 @@ class QrCodeScan extends StatefulWidget {
 }
 
 class _QrCodeScanState extends State<QrCodeScan> {
+  var data;
+
+  Future<void> scanBarcode() async {
+    try {
+      data = await BarcodeScanner.scan();
+
+      if (data.rawContent.isNotEmpty) {
+        print('Scanned Data: ${data.rawContent}');
+      } else {
+        print('No barcode scanned.');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: CustomAppBar(title: "QR Code Scanner"),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Text(data),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomElvetedButton(
+                  text: "Scan QR Code",
+                  onPressed: () {
+                    setState(() {
+                      scanBarcode();
+                    });
+                  }),
+            ],
+          ),
+        ));
   }
 }
